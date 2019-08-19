87a8aeb870bf98997136b3078986f5e1e74c9ff1
Gianluca Venturini <gianluca.91@gmail.com>
2019-08-18 20:15:01 -0700

    Fetch all ticks from polygon data source

--- src/data_source/polygon.py
+++ src/data_source/polygon.py
@@ -3,10 +3,10 @@ import requests
 import pickle
 from datetime import datetime
 from pandas import DataFrame, HDFStore, concat, pivot
+from util.cache_util import get_cached_dataframe, get_cached_dict
 
 MAX_PAGES = 1000
 BASE_API_URL = 'https://api.polygon.io'
-CACHE_PATH = '/tmp/cache'
 
 def _format_datetime(dt):
     timestamp_ms = int(dt.timestamp() * 1000)
@@ -114,52 +114,4 @@ def get_stocks_aggregate_data(interval, start, end, api_key):
     data_pivoted = pivot(data_concat, index='t', columns='symbol')
     return data_pivoted
 
-def get_cached_dict(namespace, key, get_dict):
-    ensure_cache_path_created()
-    file_path = get_file_path(namespace, key, 'pickle')
 
-    if os.path.exists(file_path):
-        print('Load from cache {namespace} {key}'.format(namespace=namespace, key=key))
-        with open(file_path, 'rb') as f:
-            return pickle.load(f)
-
-    d = get_dict()
-    with open(file_path, 'wb') as f:
-        pickle.dump(d, f)
-
-    return d
-
-def get_cached_dataframe(namespace, key, get_df):
-    ensure_cache_path_created()
-    file_path = get_file_path(namespace, ext='h5')
-
-    with pd.HDFStore(file_path) as store:
-        if key in store.keys():
-            return store[key]
-        else:
-            df = get_df()
-            store[key] = df
-
-    return df
-
-def ensure_cache_path_created():
-    if not os.path.exists(CACHE_PATH):
-        os.makedirs(CACHE_PATH)
-
-def get_file_path(namespace, key=None, ext=None):
-    return '{path}/{namespace}_{key}.{ext}'.format(
-        path=CACHE_PATH,
-        namespace=namespace,
-        key=key or '',
-        ext=ext,
-    )
-
-
-def cache_size():
-    ensure_cache_path_created()
-    return sum(
-        os.path.getsize(os.path.join(CACHE_PATH, f))
-        for f
-        in os.listdir(CACHE_PATH)
-        if os.path.isfile(os.path.join(CACHE_PATH, f))
-    )
